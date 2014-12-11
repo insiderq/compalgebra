a3cfb910ad2a238df5b01053cd5f65dd28b72336
INSIDER <unexisted_email@example.com>
2014-12-12 01:45:03 +0300

    small fix

--- history.php
+++ history.php
@@ -59,16 +59,16 @@
 
 	function handle_graph($mysqli, $expr, $result_type, $result)
 	{
-		dbresult = $mysqli->query("INSERT INTO `history`(`type`, `expr`, `result_type`, `result`) 
+		$dbresult = $mysqli->query("INSERT INTO `history`(`type`, `expr`, `result_type`, `result`) 
 			VALUES ('graph',"+$expr+","+$result_type+","+$result+","+$result+")");
-		var_dump(dbresult);
+		var_dump($dbresult);
 		echo json_encode(array("status" => "success"));
 	}
 	function handle_sqare($mysqli,$expr, $result_type, $result)
 	{
-		dbresult =$mysqli->query("INSERT INTO `history`(`type`, `expr`, `result_type`, `result`) 
+		$dbresult =$mysqli->query("INSERT INTO `history`(`type`, `expr`, `result_type`, `result`) 
 			VALUES ('square',"+$expr+","+$result_type+","+$result+","+$result+")");
-		var_dump(dbresult);
+		var_dump($dbresult);
 		echo json_encode(array("status" => "success"));
 	}
 ?>
