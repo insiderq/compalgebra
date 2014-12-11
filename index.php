b44e46d6b962633b884a08074fc6188292e2925e
INSIDER <unexisted_email@example.com>
2014-12-12 00:27:11 +0300

    second stage

---
+++ index.php
@@ -0,0 +1,7 @@
+<?
+	$mysqli = new mysqli("localhost", "root", "pass", "compalgebra");
+	if ($mysqli->connect_errno) {
+    	echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+	echo $mysqli->host_info . "\n";
+?>
