5f97e7968eca0adbc8f9a5f3ab0f202cfcc86a66
INSIDER <unexisted_email@example.com>
2014-12-12 06:10:18 +0300

    small fix

--- history.php
+++ history.php
@@ -61,9 +61,32 @@
 	{
 		$dbresult =$mysqli->query("INSERT INTO `history` (`type`, `expr`, `result_type`, `result`) 
 			VALUES ('graph','".$expr."', '".$result_type."', '".$result."')");
-		var_dump($dbresult);
-		var_dump($mysqli->insert_id);
-		//create tex and pdf files
+		if (!$dbresult){
+			echo json_encode(array("status" => "error", "reason" => "Database Problem"));
+			return;
+		}
+		$id = $mysqli->insert_id;
+
+		$texfile = fopen($id."tex","w");
+		fwrite($texfile, "\documentclass[\noneside,\n11pt, a4paper,\nfootinclude=true,\nheadinclude=true,\ncleardoublepage=empty\n]{scrbook}");
+		fwrite($texfile, "\usepackage[pdftex]{graphicx}\n\begin{document}");
+		fwrite($texfile, "\center{\\textbf{\huge Expression $$".$expr."$$}}");
+		fwrite($texfile, "\center{\\textbf{\huge Result}}");
+		switch ($res) {
+			case 'success':
+				$file = fopen($id."svg","w");
+				fwrite($file, $result);
+				fclose($file);
+				shell_exec("inkscape --export-pdf=".$id."_svg.pdf --file=".$id."svg");
+				fwrite($texfile, "\center{\includegraphics[scale=0.75]{".$id."_svg.pdf}}}");
+				break;
+			case 'error':
+				fwrite($texfile, "\center{\huge Error: ".$result."}");
+				break;
+		}
+		fwrite($texfile, "\end{document}");
+		fclose($texfile);
+		shell_exec("/usr/bin/pdflatex --interaction batchmode ".$id.".tex");
 		echo json_encode(array("status" => "success"));
 	}
 	function handle_sqare($mysqli,$expr, $result_type, $result)
