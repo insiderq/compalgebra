e9ecc82a4065f7f46b2a980f034af777f75de458
INSIDER <unexisted_email@example.com>
2014-12-12 18:11:25 +0300

    small fix

--- history_list.php
+++ history_list.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Expressions Query History</title>
+</head>
+<body>
+<table>
+<tr>
+    <th>Query ID</th>
+    <th>Expression</th>
+    <th>Result Status</th>
+    <th colspan="2">Result</th>
+</tr>
 <?php
 	error_reporting (E_ALL);
 	$mysqli = new mysqli("localhost", "root", "pass", "compalgebra");
@@ -17,7 +30,32 @@
 	$offset = ($p-1)*$limit;
 	$q = "SELECT * FROM `history` ORDER BY `id` DESC LIMIT ".$offset.",".$limit.";";
 	$rows = $mysqli->query($q);
-	while( $row = mysqli_fetch_assoc($rows) ){ 
-        var_dump($row);
+	while( $row = mysqli_fetch_assoc($rows) ){
+		$id = $row['id'];
+		$type = $row['type'];
+		$expr = $row['expr'];
+		$result_type = $row['result_type'];
+		$result = $row['result'];
+        switch ($type) {
+        	case 'graph':
+        		if ($result_type == "success"){
+        			echo "<tr><td>$id</td><td>$expr</td><td>Success</td><td><a>link 1</a></td><td><a>link 2</a></td></tr>";
+        		} else {
+        			echo "<tr><td>$id</td><td>$expr</td><td>Success</td><td colspan=\"2\">Error: $result</td></tr>";
+        		}
+        		break;
+        	
+        	case 'square':
+        		# code...
+        		break;
+        	
+
+        	default:
+        		# code...
+        		break;
+        }
     } 
 ?>
+</table>
+</body>
+</html>>
